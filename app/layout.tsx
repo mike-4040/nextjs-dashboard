02458c75f9a07bb92403d1a7260b23debc5b7122
Mike Kravtsov <mike.kravtsov@homebot.ai>
2024-02-05 19:24:12 -0800

    4. Creating Layouts and Pages

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,11 +1,8 @@
 import '@/app/ui/global.css';
 import { inter } from '@/app/ui/fonts';
+import { LayoutProps } from '../.next/types/app/layout';
 
-export default function RootLayout({
-  children,
-}: {
-  children: React.ReactNode;
-}) {
+export default function RootLayout({ children }: LayoutProps) {
   return (
     <html lang="en">
       <body className={`${inter.className} antialiased`}>{children}</body>
